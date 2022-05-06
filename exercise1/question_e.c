84fe7f1ca55189d17e2017ffafa938c098d57440
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2022-05-06 14:59:26 +0200

    change variable types
    changed from float to double due to size issues

--- exercise1/question_e.c
+++ exercise1/question_e.c
@@ -5,25 +5,26 @@ void main()
     int number_of_iterations = 0;
     float drop_volume = 0.9389;
     int report_interval = 1;
-    float total_drop_volume_multiplied = 0;
-    float total_drop_volume_accumulated = 0;
+    double total_drop_volume_multiplied = 0;
+    double total_drop_volume_accumulated = 0;
 
     printf("Please provide number of iterations:  ");
-    scanf("%d", &number_of_iterations)
-    ;
+    scanf("%d", &number_of_iterations)    ;
     printf("Please provide reporting interval:  ");
     scanf("%d", &report_interval);
 
     int i = 0;
     printf("Reporting interval is %d\n", report_interval);
-    for (i = 0; i < number_of_iterations; i++)
+    for (i = 1; i < number_of_iterations + 1; i++)
     {
-        total_drop_volume_multiplied = (i + 1) * drop_volume;
+        total_drop_volume_multiplied = i * drop_volume;
         total_drop_volume_accumulated = total_drop_volume_accumulated + drop_volume;
         if (i % report_interval == 0)
         {
-           printf("Iteration: %d  total drop volume: %f  (multiplied)\n", i, total_drop_volume_multiplied);
-           printf("Iteration: %d  total drop volume: %f  (accumulated)\n", i, total_drop_volume_accumulated);
+           printf("Iteration: %d  total drop volume: %e  (multiplied)\n", i, total_drop_volume_multiplied);
+           printf("Iteration: %d  total drop volume: %e  (accumulated)\n", i, total_drop_volume_accumulated);
         }
     }
+    printf("Absolute error: %e\n", total_drop_volume_multiplied - total_drop_volume_accumulated);
+    printf("Relative error: %e\n", (total_drop_volume_multiplied - total_drop_volume_accumulated) / total_drop_volume_accumulated);
 }
